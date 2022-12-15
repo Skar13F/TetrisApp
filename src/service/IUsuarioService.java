a22ed41ef9e22ded11a6e7bb0a6c56241e9fc4a9
elioenai46 <elio.antonio.654@gmail.com>
2022-12-15 17:18:44 -0600

    comimt service Interfaz usuario fallido

--- src/service/IUsuarioService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import entity.Usuario;
-import service.base.IRegistroServiceBase;
-
-public interface IUsuarioService extends IRegistroServiceBase<Usuario> {
-
-    public Usuario buscarRegistro(Usuario usuario);
-}
