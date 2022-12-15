514b4709c0a50b26ba6084b31255f6ba389049ee
Oscar <of5162@gmail.com>
2022-12-15 16:16:46 -0600

    commit interfaz entity

---
+++ src/entity/base/IRegistroEntityBase.java
@@ -0,0 +1,17 @@
+/**
+ * Autor: Oscar Fuentes Alvarado
+ * Fecha de creación: 28 de noviembre de 2022
+ * Fecha de actualización: 06 de diciembre de 2022
+ * Descripción: interfaz para gestionar id
+ */
+package entity.base;
+
+public interface IRegistroEntityBase {
+
+    /**
+     * Declaración de métodos abstratos.
+     */
+    public int getId();
+
+    public void setId(int id);
+}
