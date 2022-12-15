021bca0d4245b9c40043592353ec3c8c083dd131
elioenai46 <elio.antonio.654@gmail.com>
2022-12-15 16:59:36 -0600

    comimt service Interfaz jugador

---
+++ src/service/IJugadorService.java
@@ -0,0 +1,14 @@
+/**
+ * Autor: Oscar Fuentes Alvarado
+ * Fecha de creación: 28 de noviembre de 2022
+ * Fecha de actualización: 06 de diciembre de 2022
+ * Descripción: interfaz para la clase jugador controller.
+ */
+package service;
+
+import entity.Jugador;
+import service.base.IRegistroServiceBase;
+
+public interface IJugadorService extends IRegistroServiceBase<Jugador> {
+
+}
