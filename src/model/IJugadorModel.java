9d0cabe3aa34ea77e4ba591604c628993fa0ed59
Oscar <of5162@gmail.com>
2022-12-15 16:20:52 -0600

    commit model interfaz jugador

---
+++ src/model/IJugadorModel.java
@@ -0,0 +1,26 @@
+/**
+ * Autor: Oscar Fuentes Alvarado
+ * Fecha de creación: 28 de noviembre de 2022
+ * Fecha de actualización: 06 de diciembre de 2022
+ * Descripción: interfaz para el model de la clase jugador.
+ */
+package model;
+
+import entity.Jugador;
+import java.util.List;
+
+public interface IJugadorModel {
+
+    /**
+     * Declaración de métodos abstractos
+     */
+    public void insertarRegistro(Jugador jugador);
+
+    public List<Jugador> obtenerRegistro();
+
+    public void eliminarRegistro(int id);
+
+    public Jugador buscarRegistro(String telefono);
+
+    public void actualizarRegistro(Jugador jugador);
+}
