4cbf7b66a8d9038a8539fc0cc69c4d5638f2fc8a
Skar13F <of5162@gmail.com>
2022-12-15 20:34:25 -0600

    se añadió la clase app para ejecutar el programa en paquete principal

---
+++ src/principal/App.java
@@ -0,0 +1,29 @@
+/**
+ * Autor: Nancy Obed Martínez Miguel
+ * Fecha de creación: 04 de diciembre de 2022
+ * Fecha de actualización: 05 de diciembre de 2022
+ * Descripción: desde aquí se instanciará a la clase logueo para iniciar el
+ *              programa.
+ */
+package principal;
+
+import javax.swing.SwingUtilities;
+import view.PlantillaLogueo;
+
+public class App {
+
+    public static void main(String[] args) {
+        Runnable runApplication = new Runnable() {
+
+            @Override
+            public void run() {
+                PlantillaLogueo logueo = new PlantillaLogueo();
+                logueo.getClass();
+
+            }
+        };
+        SwingUtilities.invokeLater(runApplication);
+
+    }
+
+}
