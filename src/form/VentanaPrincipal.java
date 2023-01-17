de803238b87fb8a212db05b4f21bd9fadc66f227
Niky11 <nancyobed11@gmail.com>
2023-01-17 16:26:03 -0600

    Realizando la documentación de las clases

--- src/form/VentanaPrincipal.java
+++ src/form/VentanaPrincipal.java
@@ -1,3 +1,9 @@
+/**
+ * Autor: Oscar Fuentes Alvarado y Nancy Obed Martínez Miguel
+ * Fecha de creación: 10 de enero del 2023
+ * Fecha de actualización:17 de enero del 2023
+ * Descripcion :
+ */
 package form;
 
 import java.awt.Toolkit;
--- src/segregation/Drawable.java
+++ src/segregation/Drawable.java
@@ -1,4 +1,10 @@
-package segregation;
+
+/**
+ * Autor: Oscar Fuentes Alvarado y Nancy Obed Martínez Miguel
+ * Fecha de creación: 10 de enero del 2023
+ * Fecha de actualización:17 de enero del 2023
+ * Descripcion : se implementa una interfaz para dibujar
+ */package segregation;
 
 import java.awt.Graphics;
 
