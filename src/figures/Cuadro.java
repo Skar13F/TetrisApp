dc2803544b501bf654829ec80b72912dbe07b45f
Niky11 <nancyobed11@gmail.com>
2023-01-19 15:21:53 -0600

    Documentando las clases

--- src/figures/Cuadro.java
+++ src/figures/Cuadro.java
@@ -9,133 +9,136 @@ package figures;
 import java.awt.Color;
 
 public class Cuadro {
-        /**
-         * posición de cada cuadro
-         */
-	private int x, y;
-        
-        /**
-         * posiciones que ocupa cada cuadro en la matriz
-         */
-	private int indiceI, indiceJ;
-	private Color color;
-        /**
-         * Ancho y alto de cada cuadro que forma la figura
-         */
-	int ancho;
-        int alto;
 
-	public Cuadro() {
+    /**
+     * posición de cada cuadro
+     */
+    private int x, y;
 
-	}
+    /**
+     * posiciones que ocupa cada cuadro en la matriz
+     */
+    private int indiceI, indiceJ;
+    private Color color;
+    /**
+     * Ancho y alto de cada cuadro que forma la figura
+     */
+    int ancho;
+    int alto;
 
-        /**
-         * Método para darle un color, un ancho y alto a cada cuadro
-         * que posteriormente se integra para crear la figura indicada
-         */
-	public Cuadro(Color color, int ancho, int alto) {
-		this.color = color;
-		this.ancho = ancho;
-		this.alto = alto;
-	}
+    public Cuadro() {
 
-	public int getX() {
-		return x;
-	}
+    }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    /**
+     * Método para darle un color, un ancho y alto a cada cuadro que
+     * posteriormente se integra para crear la figura indicada
+     */
+    public Cuadro(Color color, int ancho, int alto) {
+        this.color = color;
+        this.ancho = ancho;
+        this.alto = alto;
+    }
 
-	public int getY() {
-		return y;
-	}
+    /**
+     * Métodos set y get
+     */
 
-	public void setY(int y) {
-		this.y = y;
-	}
+    public int getX() {
+        return x;
+    }
 
-	public int getIndiceI() {
-		return indiceI;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public void setIndiceI(int indiceI) {
-		this.indiceI = indiceI;
-	}
+    public int getY() {
+        return y;
+    }
 
-	public int getIndiceJ() {
-		return indiceJ;
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public void setIndiceJ(int indiceJ) {
-		this.indiceJ = indiceJ;
-	}
+    public int getIndiceI() {
+        return indiceI;
+    }
 
-	public Color getColor() {
-		return color;
-	}
+    public void setIndiceI(int indiceI) {
+        this.indiceI = indiceI;
+    }
 
-	public void setColor(Color color) {
-		this.color = color;
-	}
+    public int getIndiceJ() {
+        return indiceJ;
+    }
 
-	public int getAncho() {
-		return ancho;
-	}
+    public void setIndiceJ(int indiceJ) {
+        this.indiceJ = indiceJ;
+    }
 
-	public void setAncho(int ancho) {
-		this.ancho = ancho;
-	}
+    public Color getColor() {
+        return color;
+    }
 
-	public int getAlto() {
-		return alto;
-	}
+    public void setColor(Color color) {
+        this.color = color;
+    }
 
-	public void setAlto(int alto) {
-		this.alto = alto;
-	}
-	
-        /**
-         * Método para ubicar cada cuadro en la matriz del tetris
-         */
-	public Cuadro setUbicacion(int baseX, int baseY) {		
-		this.x = baseX + this.indiceI * ancho;
-		this.y = baseY + this.indiceJ * alto;
-		
- 		return this;
-	}
-        
-        /**
-         * Método para obtener la sección donde se ubica
-         * cada casilla de la figura, para posteriormente
-         * colorear el área
-         */
-	public Cuadro setUbicacion(int i, int j, int baseX, int baseY) {
-		this.indiceI = i;
-		this.indiceJ = j;
-		
-		this.x = baseX + this.indiceI * ancho;
-		this.y = baseY + this.indiceJ * alto;
-		
- 		return this;
-	}
+    public int getAncho() {
+        return ancho;
+    }
 
-        /**
-         * Método para clonar un cuadro previamente creado
-         * para después manipularlo sin alterar el original
-         * se hace una copia seteando los mismos valores
-         */
-	public Cuadro clonar() {
-		Cuadro square = new Cuadro();
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
 
-		square.setColor(this.color);
-		square.setIndiceI(this.indiceI);
-		square.setIndiceJ(this.indiceJ);
-		square.setAncho(this.ancho);
-		square.setAlto(this.alto);
-		square.setX(this.x);
-		square.setY(this.y);
-		
-		return square;
-	}
+    public int getAlto() {
+        return alto;
+    }
+
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+
+    /**
+     * Método para ubicar cada cuadro en la matriz del tetris
+     */
+    public Cuadro setUbicacion(int baseX, int baseY) {
+        this.x = baseX + this.indiceI * ancho;
+        this.y = baseY + this.indiceJ * alto;
+
+        return this;
+    }
+
+    /**
+     * Método para obtener la sección donde se ubica cada casilla de la figura,
+     * para posteriormente colorear el área
+     */
+    public Cuadro setUbicacion(int i, int j, int baseX, int baseY) {
+        this.indiceI = i;
+        this.indiceJ = j;
+
+        this.x = baseX + this.indiceI * ancho;
+        this.y = baseY + this.indiceJ * alto;
+
+        return this;
+    }
+
+    /**
+     * Método para clonar un cuadro previamente creado para después manipularlo
+     * sin alterar el original se hace una copia seteando los mismos valores
+     */
+    public Cuadro clonar() {
+        Cuadro square = new Cuadro();
+
+        square.setColor(this.color);
+        square.setIndiceI(this.indiceI);
+        square.setIndiceJ(this.indiceJ);
+        square.setAncho(this.ancho);
+        square.setAlto(this.alto);
+        square.setX(this.x);
+        square.setY(this.y);
+
+        return square;
+    }
 }
