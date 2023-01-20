86192e2d279fe578d14ab40e82d527c25c33faac
Niky11 <nancyobed11@gmail.com>
2023-01-20 14:47:13 -0600

    poniendo encabezados

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -1,3 +1,9 @@
+/**
+ * Autor: Oscar Fuentes Alvarado y Nancy Obed Martínez Miguel
+ * Fecha de creación: 04 de enero del 2023
+ * Fecha de actualización:20 de enero del 2023
+ * Descripcion : Esta ventana se ejecuta el programa
+ */
 package main;
 
 import form.VentanaPrincipal;
