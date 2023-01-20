d1159da968b98e01861f63c91bc93c0c730bb1eb
Niky11 <nancyobed11@gmail.com>
2023-01-20 14:39:56 -0600

    comentando las clases

--- src/enummanager/EnumFigure.java
+++ src/enummanager/EnumFigure.java
@@ -1,21 +1,31 @@
- package enummanager;
+/**
+ * Autor: Oscar Fuentes Alvarado y Nancy Obed Martínez Miguel
+ * Fecha de creación: 04 de enero del 2023
+ * Fecha de actualización:20 de enero del 2023
+ * Descripcion :Se implementa una clase de tipo enumerada para que se creen
+ *              objetos ya especificados en la clase
+ */
+package enummanager;
 
 import static enummanager.EnumFigure.values;
 import java.util.Random;
 
 public enum EnumFigure {
-	O_CUADRO,
-	I_SQUARE,
-	L_SQUARE,
-	J_SQUARE,
-	T_SQUARE,
-	Z_SQUARE,
-	S_SQUARE;
-	
+    O_CUADRO,
+    I_SQUARE,
+    L_SQUARE,
+    J_SQUARE,
+    T_SQUARE,
+    Z_SQUARE,
+    S_SQUARE;
+
     private static final Random PRNG = new Random();
 
-    public static EnumFigure getRandomFigure()  {
-    	EnumFigure[] directions = values();
+    /*
+     *Método que genera figuras aleatorias
+     */
+    public static EnumFigure getRandomFigure() {
+        EnumFigure[] directions = values();
         return directions[PRNG.nextInt(directions.length)];
     }
 }
--- src/enummanager/Posicion.java
+++ src/enummanager/Posicion.java
@@ -1,3 +1,10 @@
+/**
+ * Autor: Oscar Fuentes Alvarado y Nancy Obed Martínez Miguel
+ * Fecha de creación: 04 de enero del 2023
+ * Fecha de actualización:20 de enero del 2023
+ * Descripcion :Se implementa una clase de tipo enumerada para que solo se 
+ *              se utilizen esas posiciones en la clase
+ */
 package enummanager;
 
 import static enummanager.Posicion.values;
